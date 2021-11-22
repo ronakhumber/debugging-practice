0f8a066f5b8b25621edb06e2080315e5d4cbd919
Ronak Sakhuja <ronaksakhuja@Ronaks-MacBook-Air.local>
2021-11-22 13:36:00 +0530

    Added ability to select two cards

--- exercise/scripts/main.js
+++ exercise/scripts/main.js
@@ -15,12 +15,13 @@ window.addEventListener('DOMContentLoaded', function() {
       // collection of selected cards and apply the correct CSS class.
       if (selectedCards.length < 2) {
         card.classList.add('is-selected');
+        selectedCards.push(card)
       }
 
       // If we have selected two cards, see if they match.
-      if (selectedCards.length === 2) {
+      else if (selectedCards.length === 2) {
         var card1 = selectedCards[0];
-        var card2 = selectedCards[0];
+        var card2 = selectedCards[1];
 
         // If the cards match, add them to the collection of matched cards and
         // apply the correct CSS class.
@@ -33,7 +34,7 @@ window.addEventListener('DOMContentLoaded', function() {
         // Regardless of whether or not the cards match, deselect them and reset
         // the collection of matched cards.
         card1.classList.remove('is-selected');
-        card3.classList.remove('is-selected');
+        card2.classList.remove('is-selected');
         selectedCards = [];
       }
 
